3cc396ffe9bb217fa5e440f613c77a90744317db
Сергей Высоцкий <vysotskiyserhiy@gmail.com>
2018-06-07 20:06:44 +0300

    Initial Commit

---
+++ NewEquatable/NewEquatable.h
@@ -0,0 +1,19 @@
+//
+//  NewEquatable.h
+//  NewEquatable
+//
+//  Created by Serhiy Vysotskiy on 6/7/18.
+//  Copyright © 2018 Serhiy Vysotskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewEquatable.
+FOUNDATION_EXPORT double NewEquatableVersionNumber;
+
+//! Project version string for NewEquatable.
+FOUNDATION_EXPORT const unsigned char NewEquatableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewEquatable/PublicHeader.h>
+
+
